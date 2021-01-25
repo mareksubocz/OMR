83ed87b0d3c34c43fca66c09048a3ffb7bece0a6
Marek Subocz <mrksubocz@gmail.com>
2021-01-25 03:39:15 +0100

    small bugfixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Main from "./Main"
-import Loading from "./Loading"
 import './style.css'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-} from "react-router-dom";
 
 // JSX
 ReactDOM.render(
     <div>
-        <Router>
-            <Switch>
-                <Route path="/loading" component = {Loading}/>
-                <Route exact path="/" component = {Main}/>
-            </Switch>
-                {/* <Link to="/loading"> Loading </Link> */}
-                {/* <Link to="/"> Main </Link> */}
-        </Router>
-        {/* <Main/> */}
+        <Main/>
     </div>,
     document.getElementById("root")
 )
-
-var myNewP = document.createElement("p")
-myNewP.innerHTML = "This is a paragraph."
